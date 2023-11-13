88376532e4937d461a12c0d7c9cbda2f15458f7f
Oscar <myllargu@online.no>
2023-11-13 16:56:41 +0100

    Initial Commit

---
+++ Ratatouille/RatatouilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatatouilleApp.swift
+//  Ratatouille
+//
+//  Created by oscar student on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RatatouilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
