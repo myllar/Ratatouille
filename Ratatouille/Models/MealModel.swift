15d55d541ec499215752cc953a78c75d0569b339
Oscar <myllargu@online.no>
2023-11-29 12:59:34 +0100

    .

--- Ratatouille/Models/MealModel.swift
+++ Ratatouille/Models/MealModel.swift
@@ -40,14 +40,15 @@ struct MealItem: Codable, Identifiable {
         case strMeasure
 
         case ingredient
-                
-        case strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20
-        
-        case strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20
+//                
+//        case strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20
+//        
+//        case strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20
     }
     
     
-    //  SHORTHAND?
+
+//    DECODE TABEL COLUMNS FROM THE API
     
     init(from mealItemDecoder: Decoder) throws {
         let mealItemContainer = try mealItemDecoder.container(keyedBy: CodingKeys.self)
@@ -58,14 +59,11 @@ struct MealItem: Codable, Identifiable {
         strCategory = try mealItemContainer.decode(String.self, forKey: .strCategory)
         strInstructions = try mealItemContainer.decode(String.self, forKey: .strInstructions)
         strMealThumb = try mealItemContainer.decode(String.self, forKey: .strMealThumb)
-        
-                strIngredient = try mealItemContainer.decodeIfPresent(String.self, forKey: .strIngredient)
-                strMeasure = try mealItemContainer.decodeIfPresent(String.self, forKey: .strMeasure)
+        strIngredient = try mealItemContainer.decodeIfPresent(String.self, forKey: .strIngredient)
+        strMeasure = try mealItemContainer.decodeIfPresent(String.self, forKey: .strMeasure)
         
 //                isFavorite = try mealItemContainer.decode(Bool.self, forKey: .isFavorite)
 //                isArchived = try mealItemContainer.decode(Bool.self, forKey: .isArchived)
-    
-        
         
         ingredient = (1...20).compactMap { index in guard
             let name = try? mealItemContainer.decodeIfPresent(String.self, forKey: CodingKeys(rawValue: "strIngredient\(index)")!),
@@ -76,12 +74,11 @@ struct MealItem: Codable, Identifiable {
         }
             return (name, measure)
         }
-        
-        
     }
         
         
-        
+//ENCODE DATA IN THE LOCAL TABLE, INCLUDING ARCHIVE AND FAVORITE STATUS
+    
         func encode(to encoder: Encoder) throws {
                 var container = encoder.container(keyedBy: CodingKeys.self)
 
@@ -97,10 +94,8 @@ struct MealItem: Codable, Identifiable {
             }
     }
     
-    
-    
-    
-        //  EDIT PRESENTATION OF INGREDIENT+MEASURE
+
+//  EDIT PRESENTATION OF INGREDIENT+MEASURE
     extension MealItem {
         func measuredIngredient() -> String {
             return ingredient?.map {
